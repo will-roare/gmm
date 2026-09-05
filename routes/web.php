8a339d49310085b64af8bd37c9452a50cc21d460
will-roare <wilde.roare@gmail.com>
2026-09-05 19:19:59 +0200

    feat: route home page through WelcomeController

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [WelcomeController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return view('userzone.dashboard');
